3a8f71a975262b0d1a3abd27b3415999d528353a
Alessandro Argenziano <1603alessandro@gmail.com>
2025-04-06 21:15:24 +0200

    added players and a button to add new players

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { log } from 'console';
-import { url } from 'inspector';
 import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [PlayerComponent, MatButtonModule, MatIconModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
@@ -29,11 +30,13 @@ export class GameComponent {
   takeCard() {
     if (!this.pickCardAnimation) {
       this.currentCard = this.game.stack.pop();
-      console.log(this.currentCard);
       this.pickCardAnimation = true;
       setTimeout(() => {
+        if (this.currentCard) {
+          this.game.playedCards.push(this.currentCard);
+        }
         this.pickCardAnimation = false;
-      }, 1500);
+      }, 1000);
     }
   }
 }
