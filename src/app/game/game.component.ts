c1a2f0becfba48a5947393ba568a29f02dab6a3a
Alessandro Argenziano <1603alessandro@gmail.com>
2025-04-06 19:24:41 +0200

    added cards and some animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { log } from 'console';
+import { url } from 'inspector';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
+  cards = [0, 1, 2, 3];
+  card = '';
+  pickCardAnimation = false;
+  currentCard: string | undefined = '';
+  game!: Game;
 
+  constructor() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
+  }
 }
---
+++ src/models/game.ts
@@ -0,0 +1,23 @@
+export class Game {
+  public players: string[] = [];
+  public stack: string[] = [];
+  public playedCards: string[] = [];
+  public currentPlayer: number = 0;
+
+  constructor() {
+    for (let i = 1; i < 14; i++) {
+      this.stack.push('ace_' + i);
+      this.stack.push('clubs_' + i);
+      this.stack.push('hearts_' + i);
+      this.stack.push('diamonds_' + i);
+    }
+    this.shuffleStack();
+  }
+
+  private shuffleStack(): void {
+    for (let i = this.stack.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.stack[i], this.stack[j]] = [this.stack[j], this.stack[i]];
+    }
+  }
+}
