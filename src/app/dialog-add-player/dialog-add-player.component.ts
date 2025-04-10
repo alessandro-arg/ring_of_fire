af49c63c5747f5b7c10ccfc531501edb51395cf8
Alessandro Argenziano <1603alessandro@gmail.com>
2025-04-10 13:18:24 +0200

    added the current player and changed some css

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-dialog-add-player',
   standalone: true,
-  imports: [MatDialogModule, FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './dialog-add-player.component.html',
   styleUrl: './dialog-add-player.component.scss',
 })
-export class DialogAddPlayerComponent {}
+export class DialogAddPlayerComponent {
+  name: string = '';
+
+  onNoClick() {}
+}
